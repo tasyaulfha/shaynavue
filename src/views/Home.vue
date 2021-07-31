2e7e868aebe63d27f89e1b0c78e6244891e6b478
TASYA <17523184@gmail.com>
2021-07-31 14:59:44 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna  />
+    <WomenShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import HeaderShayna from "../components/HeaderShayna.vue";
+//import carousel from "vue-owl-carousel";
+import HeroShayna from "../components/HeroShayna.vue"
+import WomenShayna from "../components/WomenShayna.vue"
+import InstaShayna from "../components/InstaShayna.vue"
+import PartnerShayna from "../components/PartnerShayna.vue"
+import Footer from "../components/Footer.vue"
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+ //   carousel,
+    HeroShayna,
+    WomenShayna,
+    InstaShayna,
+    PartnerShayna,
+    Footer
+  },
+};
+</script>
